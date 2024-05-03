9c8bafd11427fb30bed1d891f96d9468aa81afce
bnkaziz <abdelazizbenkhaldi1@gmail.com>
2024-05-03 11:44:14 +0300

    redis url from localhost to 127.0.0.1

--- server/services/redis.js
+++ server/services/redis.js
@@ -2,7 +2,7 @@ const redis = require("redis");
 
 // Redis initialisation
 const redisClient = redis.createClient({
-  url: "redis://localhost:6379",
+  url: "redis://127.0.0.1:6379",
 });
 
 const start = async () => {
