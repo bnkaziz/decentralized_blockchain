df9a529481221b3e2062943e85e93f9fb9448d56
bnkaziz <abdelazizbenkhaldi1@gmail.com>
2024-05-03 20:15:06 +0300

    logs for cookies & sessions

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -1,4 +1,7 @@
 const authenticate = (req, res, next) => {
+  console.log("Authenticate: ");
+  console.log("Session: ", req.session);
+  console.log(`User_id: ${req.session.user_id}`);
   if (!req.session || !req.session.user_id) {
     const err = new Error("Not authorized!");
     err.statusCode = 401;
