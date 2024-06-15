f8653638d0a3ed674ee18986ac2428ba81a287a8
bnkaziz <abdelazizbenkhaldi1@gmail.com>
2024-06-16 00:54:24 +0300

    domain

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -10,7 +10,7 @@ var whitelist = new Set([
   // "http://127.0.0.1:9982",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "https://magical-pegasus-f42cbc.netlify.app",
+  "https://centralizedrefundsystem.netlify.app",
 ]);
 
 const corsOptions = {
